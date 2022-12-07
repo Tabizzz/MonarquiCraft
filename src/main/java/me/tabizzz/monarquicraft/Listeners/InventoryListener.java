bae5e3e0667de5662ce9ab04f30eab8fc4234be5
Tabizzz <tabatha.reds@gmail.com>
2022-12-06 23:07:21 -0500

    Update InventoryListener.java
    Convert to MCItems items in creative

--- src/main/java/me/tabizzz/monarquicraft/Listeners/InventoryListener.java
+++ src/main/java/me/tabizzz/monarquicraft/Listeners/InventoryListener.java
@@ -6,6 +6,7 @@ import me.tabizzz.monarquicraft.Utils.MCItemUtils;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.enchantment.EnchantItemEvent;
+import org.bukkit.event.inventory.InventoryCreativeEvent;
 import org.bukkit.event.inventory.PrepareAnvilEvent;
 import org.bukkit.event.inventory.PrepareItemCraftEvent;
 import org.bukkit.event.inventory.PrepareSmithingEvent;
@@ -61,5 +62,13 @@ public class InventoryListener implements Listener {
 		if(res == null) return;
 		if (MCItemUtils.canBeMCItem(res))
 			event.getInventory().setResult(MCItemUtils.createFromVanilla(res).getItem());
+	}
+
+	@EventHandler
+	public void OnCreative(InventoryCreativeEvent event) {
+		var item = event.getCursor();
+		if (item == null) return;
+		if (MCItemUtils.canBeMCItem(item))
+			event.setCursor(MCItemUtils.createFromVanilla(item).getItem());
 	}
 }
