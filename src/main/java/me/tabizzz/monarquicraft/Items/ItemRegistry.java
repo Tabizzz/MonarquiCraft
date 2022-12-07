0cb3b9913fbbe32507faa7a776e2a3f40c905bbf
Tabizzz <tabatha.reds@gmail.com>
2022-12-06 23:40:52 -0500

    Update ItemRegistry.java
    Added support for load sub dirs

--- src/main/java/me/tabizzz/monarquicraft/Items/ItemRegistry.java
+++ src/main/java/me/tabizzz/monarquicraft/Items/ItemRegistry.java
@@ -1,6 +1,7 @@
 package me.tabizzz.monarquicraft.Items;
 
 import me.tabizzz.monarquicraft.Config.ConfigAccessor;
+import me.tabizzz.monarquicraft.MonarquiCraft;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -42,16 +43,31 @@ public class ItemRegistry {
 	public void loadFromFiles() {
 		plugin.getLogger().info("Loading Custom Items");
 		var files = new File(plugin.getDataFolder(), "Items");
+		var loaded = loadFromDirectory(files, "Items");
+		plugin.getLogger().info("Loaded " + loaded + " items");
+	}
+
+	private int loadFromDirectory(File files, String items) {
+		var loaded = 0;
 		if(files.exists() && files.isDirectory()) {
+			MonarquiCraft.getPlugin().getLogger().info("Loading files from dir: " + items);
 			var itemFiles = files.listFiles((dir, name) -> name.toLowerCase().endsWith(".yml"));
 			for (var item: itemFiles != null ? itemFiles : new File[0]) {
-				var config = new ConfigAccessor(plugin, "Items/" + item.getName());
+				var config = new ConfigAccessor(plugin, items + "/" + item.getName());
 				var id = item.getName().substring(0, item.getName().lastIndexOf('.'));
 				var newItem = ItemLoader.load(config.getConfig(), id);
-				loadedItems.put(id, newItem);
+				if(newItem != null) {
+					loaded++;
+					loadedItems.put(id, newItem);
+				}
 			}
-			plugin.getLogger().info("Loaded " + (itemFiles != null ? itemFiles.length : 0) + " items");
+
+			var itemDirs = files.listFiles((dir, name) -> !name.toLowerCase().endsWith(".yml"));
+			for (var dir: itemDirs != null ? itemDirs : new File[0]) {
+				loaded += loadFromDirectory(dir, items + "/" + dir.getName());
+			}
 		}
+		return loaded;
 	}
 
 	public Collection<String> idSet() {
